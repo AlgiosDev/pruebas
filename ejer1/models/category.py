e0def59eb5e930ca45b8ef5d2944819c7f10b5bd
aidaALGIOS <aida.gonzalez@algios.com>
2015-10-22 13:40:43 +0200

    Ejercicio 2 - Parte 3

--- ejer1/models/category.py
+++ ejer1/models/category.py
@@ -5,6 +5,19 @@ from openerp import models, fields
 class Category(models.Model):
     _name = 'openacademy.course.category'
 
-    code = fields.Char()
-    name = fields.Char()
+    code = fields.Char(required=True)
+    name = fields.Char(required=True)
     description = fields.Text()
+    
+    
+    _sql_constraints = [           
+        ('code_unique',
+         'UNIQUE(code)',
+         "The code must be unique"),
+                        
+        ('name_unique',
+         'UNIQUE(name)',
+         "The name must be unique"), 
+    ]
+    
+ 
